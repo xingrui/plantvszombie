6f4e48ad804953f8efe9fba156851555735bfbb1
xingrui05@gmail.com <xingrui05@gmail.com@461baa94-84be-cc71-52aa-179292a27d0c>
2011-08-27 11:27:56 +0000

    submit my first version of my tool

---
+++ trunk/plant/plantDlg.h
@@ -0,0 +1,40 @@
+// plantDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CplantDlg 对话框
+class CplantDlg : public CDialog
+{
+// 构造
+public:
+	CplantDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_PLANT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+protected:
+	void PressChoose(int nIndex);
+	void ChoosePlant(int nRow, int nColumn);
+	void SetPlant(int nRow, int nColumn);
+	CWnd * m_plantWnd;
+public:
+	afx_msg void OnBnClickedButton1();
+};
